2dc90da958ac3e4229963b807f9267742ff1320b
RomaSamkov <Romansamkov.rs@gmail.com>
2024-05-24 12:46:25 +0300

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <div className="flex justify-between mx-auto max-w-[1200px] sm:h-[150px] p-12">
+      <span className="primary-color">Roma RS</span>
+      <p className="text-gray-600">user@mail.com</p>
+    </div>
+  );
+};
+
+export default Footer;
