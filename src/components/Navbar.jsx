7ebcfbd2db6250d4d81b997236b14db884571120
RomaSamkov <Romansamkov.rs@gmail.com>
2024-05-27 14:09:23 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     setNav(!nav);
   };
   return (
-    <div className="bg-black text-gray-400 h-[100px] max-w-[1200px] mx-auto flex justify-between items-center">
+    <div className="text-gray-400 h-[100px] max-w-[1200px] mx-auto flex justify-between items-center">
       <h1 className="text-3xl font-bold primary-color ml-4">RomaRS</h1>
       <ul className="hidden md:flex">
         <li className="p-5">
