ba6b21e785d07cbea58c6fc5aaee23f4d2529034
RomaSamkov <Romansamkov.rs@gmail.com>
2024-05-27 14:09:18 +0300

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import heroimage from "../assets/CV-image.png";
 const Hero = () => {
   const text = "I'm a";
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-3 gap-8 max-w-[1200px] md:h-[80vh] mx-auto py-8 bg-black">
+    <div className="grid grid-cols-1 sm:grid-cols-3 gap-8 max-w-[1200px] md:h-[80vh] mx-auto py-8">
       <div className="my-auto mx-auto col-span-1 w-[300px] h-auto lg:w-[400px]">
         <img
           src={heroimage}
