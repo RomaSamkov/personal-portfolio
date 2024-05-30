637bd6c6e47eeae74942db9969bf754350709ee6
RomaSamkov <Romansamkov.rs@gmail.com>
2024-05-30 12:47:16 +0300

    Update Work.jsx

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -22,14 +22,12 @@ const Work = () => {
             alt="project1"
             className="h-[100%] w-full rounded-md"
           />
-          <div className="opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col justify-center items-center">
-            <span className="p-1 text-white">
-              Project1 Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Officiis, quos ad modi voluptatem atque in ut corrupti velit
-              repellat, perferendis nobis voluptas eos maiores ertwetewrt
-              etwertwertewrtw
+          <div className="opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col justify-between items-center">
+            <span className="pl-4 text-black font-bold inline-block text-[1.1rem]">
+              2D game with vanilla JavaScript, <br /> HTML5, CSS3 and HTML
+              Canvas !
             </span>
-            <div className="pt-8 text-center">
+            <div className="text-center">
               <a href="">
                 <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                   Live
@@ -37,7 +35,7 @@ const Work = () => {
               </a>
               <a href="">
                 <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                  Live
+                  Github
                 </button>
               </a>
             </div>
@@ -48,14 +46,21 @@ const Work = () => {
         shadow-lg shadow-[#040c16] group rounded-md flex justify-center items-center h-[200px] bg-cover relative"
         >
           <img src={proj2} alt="project2" className="h-[100%]" />
-          <div className="opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col justify-center items-center">
-            <span className="text-2xl font-bold text-white tracking-wider">
-              Project2
+          <div className="opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col justify-between items-center">
+            <span className="pl-4 text-black font-bold inline-block text-[1rem] pt-2">
+              Team project about searching for movies, and adding favorite
+              movies to a personal library. Used: pure JS, npm packages and
+              SCSS. Team-project. Role: Team Lead.
             </span>
-            <div className="pt-8 text-center">
+            <div className="text-center">
               <a href="">
-                <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                <button className="text-center rounded-lg w-[100px] m-2 bg-white text-gray-700 font-bold text-lg hover:text-white hover:bg-[#110822]/50">
                   Live
+                </button>
+              </a>
+              <a href="">
+                <button className="text-center rounded-lg w-[100px] m-2 bg-white text-gray-700 font-bold text-lg hover:text-white hover:bg-[#110822]/50">
+                  Github
                 </button>
               </a>
             </div>
@@ -66,14 +71,22 @@ const Work = () => {
         shadow-lg shadow-[#040c16] group rounded-md flex justify-center items-center h-[200px] bg-cover relative"
         >
           <img src={proj3} alt="project3" className="h-[100%]" />
-          <div className="opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col justify-center items-center">
-            <span className="text-2xl font-bold text-white tracking-wider">
-              Project3
+          <div className="opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col justify-between items-center">
+            <span className="pl-4 pr-4 text-white font-bold inline-block text-[0.9rem] pt-2 font-serif">
+              Team project. Full-stack application for people with pets. Used:
+              React, Redux, RTK Query, react-router-dom, React i18n,
+              Node.JS,Express, MongoDB, Mongoose, Formik, Yup, SASS,
+              Material-UI.
             </span>
-            <div className="pt-8 text-center">
+            <div className="text-center">
               <a href="">
-                <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                <button className="text-center rounded-lg w-[100px] mr-2 mb-2  bg-white text-gray-700 font-bold text-lg hover:text-white hover:bg-[#110822]/50">
                   Live
+                </button>
+              </a>
+              <a href="">
+                <button className="text-center rounded-lg w-[100px] mr-2 mb-2  bg-white text-gray-700 font-bold text-lg hover:text-white hover:bg-[#110822]/50">
+                  Github
                 </button>
               </a>
             </div>
@@ -85,13 +98,20 @@ const Work = () => {
         >
           <img src={proj4} alt="project4" className="h-[100%]" />
           <div className="opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col justify-center items-center">
-            <span className="text-2xl font-bold text-white tracking-wider">
-              Project4
+            <span className="pl-4 text-black font-bold inline-block">
+              Application with sounds for saving phone numbers. Used: React,
+              Create React App, Redux, Redux Toolkit, redux-persist,
+              react-router-dom, Formik, Yup, Styled Components, Material-UI.
             </span>
             <div className="pt-8 text-center">
               <a href="">
                 <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                   Live
+                </button>
+              </a>
+              <a href="">
+                <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                  Github
                 </button>
               </a>
             </div>
@@ -103,13 +123,18 @@ const Work = () => {
         >
           <img src={proj5} alt="project5" className="h-[100%]" />
           <div className="opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col justify-center items-center">
-            <span className="text-2xl font-bold text-white tracking-wider">
+            <span className="pl-4 text-black font-bold inline-block">
               Project5
             </span>
             <div className="pt-8 text-center">
               <a href="">
                 <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                   Live
+                </button>
+              </a>
+              <a href="">
+                <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                  Github
                 </button>
               </a>
             </div>
@@ -121,13 +146,18 @@ const Work = () => {
         >
           <img src={proj6} alt="project6" className="h-[100%]" />
           <div className="opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col justify-center items-center">
-            <span className="text-2xl font-bold text-white tracking-wider">
+            <span className="pl-4 text-black font-bold inline-block">
               Project6
             </span>
             <div className="pt-8 text-center">
               <a href="">
                 <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                   Live
+                </button>
+              </a>
+              <a href="">
+                <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                  Github
                 </button>
               </a>
             </div>
